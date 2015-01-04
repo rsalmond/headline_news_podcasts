48f1a8df7f17ba3eb4afd20016c29df1b5f802c6
Rob Salmond <rob@salmond.ca>
2015-01-04 02:49:05 -0500

    add checks for required programs

--- get.sh
+++ get.sh
@@ -1,15 +1,29 @@
 #!/bin/bash
 
+cmd_exists() {
+    hash "$1" > /dev/null 2>&1
+}
+
+for cmd in python avconv mp3wrap lame
+do
+    if ! cmd_exists $cmd; then
+        echo "ERR: Can't find $cmd, $0 requires that $cmd is available"
+        exit 1
+    fi
+done
+
+exit 0
+
 # prep work dir 
-rm working -rf
-mkdir -p working/out
+workdir=`mktemp -d`
+mkdir $workdir/out
 
 # get dem podcasts
-echo "dloading podcasts ..."
-python grabber.py
+echo "Downloading podcasts ..."
+python grabber.py --dest=$workdir
 
 # normalize bit and sample rates
-cd working
+cd $workdir
 for f in *.mp3; do avconv -i $f -b 64k -ar 44100 -c:a pcm_s16le ./out/$f.wav; done
 
 # re encode to mp3
@@ -17,8 +31,8 @@ cd out
 for f in *.wav; do lame $f $f.mp3; done
 
 # concat
-cd ../..
-mp3wrap ./working/final.mp3 ./working/out/*.mp3
+cd $workdir
+mp3wrap final.mp3 $workdir/out/*.mp3
 
 # fucking mp3wrap shits on my filenames
 mv ./working/final_MP3WRAP.mp3 ./final.mp3
